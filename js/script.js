a341d16883a74725f8d68bcef405d5ca4a4691d6
davidecarta470 <cartadavi@gmail.com>
2021-10-29 17:38:20 +0200

    esercizio1 con funzione mia

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
-// **Palidroma**
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+// // **Palidroma**
+// // Chiedere all’utente di inserire una parola
+// // Creare una funzione per capire se la parola inserita è palindroma
 
 const parola = prompt('digita una parola');
 let palindroma = 'non è palindroma';
 console.log(reverseWords(parola));
+
 if (reverseWords(parola)===parola){
   palindroma='è palindroma';
 }
 
 console.log('la parola',palindroma);
 
+
+
+// funzione reverse
 function reverseWords(str) {
-  let reverseWordArr = str.split(" ").map(word => word.split("").reverse().join(""));
-  return reverseWordArr.join(" ");
+
+   let array = [];
+  for (let i =0 ;i <str.length;i++) {
+      array[i]=str[(str.length-1)-i];
+  }
+   return array
+
+
+
 }
 
-// **Pari e Dispari**
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
-// **Consigli del giorno**
-// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
-// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+// // **Pari e Dispari**
+// // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// // Sommiamo i due numeri
+// // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// // Dichiariamo chi ha vinto.
+// // **Consigli del giorno**
+// // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+// // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 
 // utente inserisci la stringa o 'pari' o 'dispari'
 let pariDispari = prompt('pari o dispari?');
